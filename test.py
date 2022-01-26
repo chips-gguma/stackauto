eed4c87d17f224d29e2dd7effe1808d4cfc3f0fd
chips-gguma <ljygguma@gmail.com>
2022-01-26 14:32:03 +0900

    slack bot 다시 구현

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ import requests
  
 def post_message(token, channel, text):
     response = requests.post("https://slack.com/api/chat.postMessage",
-        headers={"Authorization": "Bearer "+token},
+        headers={"Authorization": "Bearer " + token},
         data={"channel": channel,"text": text}
     )
     print(response)
  
-myToken = "xoxb-3007010903154-3009331608644-66sM6PyTSfy36F5EUsdMZHJC"
+myToken = "xoxb-3007010903154-3009331608644-FwJCOGmN0HLk9UceeXdMBJrr"
  
 post_message(myToken,"#stock","Hello World!")
