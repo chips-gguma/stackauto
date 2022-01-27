67665316d661649b342aef160c047dcb1dac345c
chips-gguma <ljygguma@gmail.com>
2022-01-27 19:17:27 +0900

    주석 추가

--- AutoTrade.py
+++ AutoTrade.py
@@ -4,10 +4,6 @@ import pandas as pd
 from datetime import datetime
 import requests
 import time, calendar
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options 
 
 def post_message(token, channel, text):
     response = requests.post("https://slack.com/api/chat.postMessage",
@@ -253,11 +249,12 @@ def sell_all():
 
 if __name__ == '__main__': 
     try:
+        # 자동 매매할 주식 종목 코드 
         symbol_list = ['A122630', 'A252670', 'A233740', 'A250780', 'A225130',
              'A280940', 'A261220', 'A217770', 'A295000', 'A176950']
         bought_list = []     # 매수 완료된 종목 리스트
         target_buy_count = 5 # 매수할 종목 수
-        buy_percent = 0.19   
+        buy_percent = 0.19   # 전체 가용 자금에서 몇 %를 매수할 것인지 (각 종목당)
         printlog('check_creon_system() :', check_creon_system())  # 크레온 접속 점검
         stocks = get_stock_balance('ALL')      # 보유한 모든 종목 조회
         total_cash = int(get_current_cash())   # 100% 증거금 주문 가능 금액 조회
@@ -266,14 +263,14 @@ if __name__ == '__main__':
         printlog('종목별 주문 비율 :', buy_percent)
         printlog('종목별 주문 금액 :', buy_amount)
         printlog('시작 시간 :', datetime.now().strftime('%m/%d %H:%M:%S'))
-        soldout = False;
+        soldout = False
 
         while True:
             t_now = datetime.now()
             t_9 = t_now.replace(hour=9, minute=0, second=0, microsecond=0)
-            t_start = t_now.replace(hour=9, minute=5, second=0, microsecond=0)
-            t_sell = t_now.replace(hour=15, minute=15, second=0, microsecond=0)
-            t_exit = t_now.replace(hour=15, minute=20, second=0,microsecond=0)
+            t_start = t_now.replace(hour=9, minute=5, second=0, microsecond=0) # LP(유동성 공급자) 활동 시간
+            t_sell = t_now.replace(hour=15, minute=15, second=0, microsecond=0) # 자동 매도 시간 
+            t_exit = t_now.replace(hour=15, minute=20, second=0,microsecond=0)  # 종료
             today = datetime.today().weekday()
             if today == 5 or today == 6:  # 토요일이나 일요일이면 자동 종료
                 printlog('Today is', 'Saturday.' if today == 5 else 'Sunday.')
